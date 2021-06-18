3fb8a659851749f952e22b6bea72fc16d7cf2006
fattmamussa <69681626+fattmamussa@users.noreply.github.com>
2021-06-18 02:16:30 -0700

    Add files via upload

---
+++ q2.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>COURSES INSTRACTOR THAT GIVE OUT</title>
+</head>
+<body>
+
+
+	<!-- navigation-->
+	<nav class="navbar navbar-expand" id="navbar">
+		<div class="container">
+			<a href="mypage.php" class="navbar-brand">MY ONLINE COURSE INSTRACTOR THAT GIVE OUT</a>
+
+			<div class="collapse navbar-collapse" id="nav-items">
+				<ul class="nav ml-auto">
+			        <li class="item active"><a href="#navbar" class="nav-link">JSP</a></li>
+					<li class="item"><a href="javascript.php" class="nav-link">JAVASCRIPT</a></li>
+					<li class="item"><a href="mypage.php" class="nav-link">HTML&CSS</a></li>
+					<li class="item"><a href="flatter.php" class="nav-link">FLATTER</a></li>
+					<li class="item"><a href="contact us.html" class="nav-link">CONTANT US</a></li>
+
+				</ul>
+			</div>
+		</div>
+    </nav>
+
+    <!-- end nav-->
+
+    <section class="ftco-wrap-about" id="about">
+			<div  class="intro">
+				<div class="container">
+					<div class="row">
+						<div class="col">
+							<div class="intro_content">
+								<div class="intro_title">
+									<h2><b>FATMA MUSSA HASSAN</b></h2>
+								</div>
+								<div class="intro_text">
+									<p>This are the online a course instructor for a course that is given online for to study JSP,HTML&CSS, Javascript and Flatter.<br>
+									contacts us by using phone number&email:<br>
+									2556453212<br>
+                                    tumandhra123@gmail.com<br>
+                                    
+									</p>
+							</div>
+						</div>
+								<button class="dropbtn"><img src="/img/user.png" class="icon" alt="image" />" </div>
+							</div>
+					        
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>							
+
+</section>
---
+++ q3.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="ne/css" href="ne.css"/>
+
+<title></title>
+</head>
+<body>
+<table border="1px;" align="center" cellspacing="0px;" width="500px;" height="200px;"> 
+ 
+<tr>
+ <td align="center">
+	<h1 font face="algerian">
+	<font style="color:blue;">G</font>
+	<font color="red">o</font>
+	<font style="color:yellow">o</font>
+	<font style="color:blue">g</font>
+	<font style="color:#00ff00">l</font>
+	<font style="color:red">e</font>
+	</h1>
+ 
+  
+ 
+ <input type="text" name="" required/><p>   
+ <input type="button" value="Google Search" required/>
+  <input type="button" value="Im feeling Lucky" required></p>
+ </td>
+ 
+ 
+ 
+ 
+ </tr>
+ </table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+
+
+
+
+
+
+
+
+
+
+</html>
